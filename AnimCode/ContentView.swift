5626210e4408e6bb5da8fd21e9153643457266f9
Ankit Singh <kislotaooankit@gmail.com>
2021-06-15 11:07:59 +0530

    Added Home Panel Lesson 7

--- AnimCode/ContentView.swift
+++ AnimCode/ContentView.swift
@@ -8,42 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var show = false
+    @State var viewState = CGSize.zero
+    
+    
     var body: some View {
         ZStack {
             
             
             TitleView()
-                .blur(radius: 20)
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
 
             
             CardBottomView()
-                .blur(radius: 20)
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
             
             CardView()
-                .offset(x:0,y:-40)
+                .background(show ? Color.red : Color("background8"))
+                .cornerRadius(10)
+                .shadow(radius: 20)
+                .offset(x:0,y: show ? -400 : -40)
                 .scaleEffect(0.85)
-                .rotationEffect(Angle(degrees: 15))
-                .rotation3DEffect(
-                    Angle(degrees: 50),
-                    axis: (x:10,y:10,z:10))
+                .rotationEffect(Angle(degrees: show ? 15 : 0))
+//                .rotation3DEffect(
+//                    Angle(degrees: show ? 50 : 0),
+//                    axis: (x:10,y:10,z:10))
+                .animation(.easeInOut(duration: 0.7))
                 .blendMode(.hardLight)
+                .offset(x:viewState.width, y: viewState.height)
+
             
             CardView()
-                .offset(x:0,y:-20)
+                
+                .background(show ? Color("background5") : Color("background8"))
+                .cornerRadius(10)
+                .shadow(radius: 20)
+                .offset(x:0,y: show ? -200 : -20)
                 .scaleEffect(0.9)
-                .rotationEffect(Angle(degrees: 10))
-                .rotation3DEffect(
-                    Angle(degrees: 40),
-                    axis: (x:10,y:10,z:10))
+                .rotationEffect(Angle(degrees: show ? 10: 0))
+//                .rotation3DEffect(
+//                    Angle(degrees: show ? 40 : 0),
+//                    axis: (x:10,y:10,z:10))
+                .animation(.easeInOut(duration: 0.5))
+
                 .blendMode(.hardLight)
+                .offset(x:viewState.width, y: viewState.height)
+
             
             
             CertificateView()
+                .offset(x:viewState.width, y: viewState.height)
                 .scaleEffect(0.95)
-                .rotationEffect(Angle(degrees: 5))
-                .rotation3DEffect(
-                    Angle(degrees: 30),
-                    axis: (x:10,y:10,z:10))
+                .rotationEffect(Angle(degrees: show ? 5:0))
+//                .rotation3DEffect(
+//                    Angle(degrees: show ? 30 : 0),
+//                    axis: (x:10,y:10,z:10))
+                .animation(.spring())
+                .onTapGesture {
+                    self.show.toggle()
+                }
+                .gesture(
+                    DragGesture()
+                        .onChanged({ Value in
+                            self.viewState = Value.translation
+                            self.show = true
+                        })
+                        .onEnded({ value in
+                            self.viewState = CGSize.zero
+                            self.show = false
+                        })
+                
+                )
             
         }//zstack
         
@@ -63,9 +101,7 @@ struct CardView: View {
             Text("Card Back")
         }//vstack
         .frame(width: 340, height: 220)
-        .background(Color.blue)
-        .cornerRadius(10)
-        .shadow(radius: 20)
+        
     }
 }
 
