1c2b38fd051303a19c945cb99ad3a75405596111
Ankit Singh <kislotaooankit@gmail.com>
2021-06-15 08:24:16 +0530

    Initial Commit
    lesson 3 completed

--- AnimCode/ContentView.swift
+++ AnimCode/ContentView.swift
@@ -9,13 +9,127 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            
+            
+            TitleView()
+                .blur(radius: 20)
+
+            
+            CardBottomView()
+                .blur(radius: 20)
+            
+            CardView()
+                .offset(x:0,y:-40)
+                .scaleEffect(0.85)
+                .rotationEffect(Angle(degrees: 15))
+                .rotation3DEffect(
+                    Angle(degrees: 50),
+                    axis: (x:10,y:10,z:10))
+                .blendMode(.hardLight)
+            
+            CardView()
+                .offset(x:0,y:-20)
+                .scaleEffect(0.9)
+                .rotationEffect(Angle(degrees: 10))
+                .rotation3DEffect(
+                    Angle(degrees: 40),
+                    axis: (x:10,y:10,z:10))
+                .blendMode(.hardLight)
+            
+            
+            CertificateView()
+                .scaleEffect(0.95)
+                .rotationEffect(Angle(degrees: 5))
+                .rotation3DEffect(
+                    Angle(degrees: 30),
+                    axis: (x:10,y:10,z:10))
+            
+        }//zstack
+        
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack{
+            Text("Card Back")
+        }//vstack
+        .frame(width: 340, height: 220)
+        .background(Color.blue)
+        .cornerRadius(10)
+        .shadow(radius: 20)
+    }
+}
+
+struct CertificateView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading)
+                {
+                    Text("UI Design")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("accent"))
+                        .font(.headline)
+                        .padding(.top)
+                    Text("Certificate")
+                        .foregroundColor(.white)
+                }//Vstack
+                Spacer()
+                Image("Logo")
+                    .resizable()
+                    .frame(width: 30.0, height: 30.0)
+            }//HStack
+            .padding(.horizontal)
+            Spacer()
+            Image("Background")
+        }//Vstack
+        .frame(width: 340, height: 220)
+        .background(Color.black)
+        .cornerRadius(10.0)
+        .shadow(radius: 20)
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificates")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                Spacer()
+            }
+            Image("Illustration5")
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct CardBottomView: View {
+    var body: some View {
+        VStack(spacing:20) {
+            Rectangle()
+                .frame(width: 60, height: 6)
+                .cornerRadius(5.0)
+                .opacity(0.1)
+            Text("This Certificate is a proof that Ankit Has achieved the UI Design Course with full distinction")
+            Spacer()
+        }
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .padding()
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(radius: 20)
+        .offset(y:600)
     }
 }
